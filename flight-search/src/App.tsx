33ada24913122653fcf4cfa9f1f31069d9f3d831
Gerardo Ramos <gerardo.ramos@encora.com>
2025-05-06 12:49:59 -0600

    Fixed dark theme

--- flight-search/src/App.tsx
+++ flight-search/src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import React from "react";
-import FlightResultsView from "./components/SearchResults/FlightResultsView";
+import FlightResultsView from "./components/FlightResults/FlightResultsView";
+import DetailsView from "./components/DetailsView/DetailsView";
 
 function App() {
   return (
     <>
-      <FlightResultsView></FlightResultsView>
+      <DetailsView></DetailsView>
     </>
   );
 }
