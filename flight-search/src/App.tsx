2ecb9c817b2000d9eaa6904ded0acf5258c0e4c0
Gerardo Ramos <gerardo.ramos@encora.com>
2025-05-06 09:44:34 -0600

    Changed to visualize centering issue

---
+++ flight-search/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import SearchView from "./components/SearchView/SearchView";
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchView></SearchView>
+    </div>
+  );
+}
+
+export default App;
