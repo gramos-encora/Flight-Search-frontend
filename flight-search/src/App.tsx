b3ac17c0644cc77b84b811bb0e1b318f8ad41455
Gerardo Ramos <gerardo.ramos@encora.com>
2025-05-06 10:22:49 -0600

    Created FlightResultsView

---
+++ flight-search/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import React from "react";
+import FlightResultsView from "./components/SearchResults/FlightResultsView";
+
+function App() {
+  return (
+    <>
+      <FlightResultsView></FlightResultsView>
+    </>
+  );
+}
+
+export default App;
